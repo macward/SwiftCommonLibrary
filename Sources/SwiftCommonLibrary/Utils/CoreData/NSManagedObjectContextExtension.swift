4116f1804c2c705ec08eab1b877d9bb8751eae22
Max Ward <ward.maximiliano@gmail.com>
2023-11-23 01:32:44 -0300

    extension fix

--- Sources/SwiftCommonLibrary/Utils/CoreData/NSManagedObjectContextExtension.swift
+++ Sources/SwiftCommonLibrary/Utils/CoreData/NSManagedObjectContextExtension.swift
@@ -8,11 +8,11 @@
 import Foundation
 import CoreData
 
-extension NSManagedObjectContext {
+public extension NSManagedObjectContext {
 
     /// Only performs a save if there are changes to commit.
     /// - Returns: `true` if a save was needed. Otherwise, `false`.
-    @discardableResult public func saveIfNeeded() throws -> Bool {
+    @discardableResult func saveIfNeeded() throws -> Bool {
         guard hasChanges else { return false }
         try save()
         return true
