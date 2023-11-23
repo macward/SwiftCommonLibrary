44f2a6c672cd1cef224d36554670b4516ec5fc90
Max Ward <ward.maximiliano@gmail.com>
2023-11-23 01:20:06 -0300

    NSManagedObjectContext saveIfNeeded

---
+++ Sources/SwiftCommonLibrary/Utils/CoreData/NSManagedObjectContextExtension.swift
@@ -0,0 +1,21 @@
+//
+//  File.swift
+//  
+//
+//  Created by Max Ward on 23/11/2023.
+//
+
+import Foundation
+import CoreData
+
+extension NSManagedObjectContext {
+
+    /// Only performs a save if there are changes to commit.
+    /// - Returns: `true` if a save was needed. Otherwise, `false`.
+    @discardableResult public func saveIfNeeded() throws -> Bool {
+        guard hasChanges else { return false }
+        try save()
+        return true
+    }
+}
+
