af4fb0251b2ccc33176f170a83f98b9aa0519a2d
Max Ward <ward.maximiliano@gmail.com>
2023-11-28 16:12:20 -0300

    OnLoad view modifier

---
+++ Sources/SwiftCommonLibrary/ViewModifiers/OnLoadViewModifier.swift
@@ -0,0 +1,29 @@
+//
+//  OnLoad.swift
+//
+//
+//  Created by Max Ward on 28/11/2023.
+//
+
+import Foundation
+import SwiftUI
+
+struct OnLoadViewModifier: ViewModifier {
+    let action: () -> Void
+    @State private var loaded: Bool = false
+    func body(content: Content) -> some View {
+        content
+            .onAppear() {
+                if !loaded {
+                    loaded = true
+                    action()
+                }
+            }
+    }
+}
+
+extension View {
+    public func onLoad(perform action: @escaping () -> Void) -> some View {
+        modifier(OnLoadViewModifier(action: action))
+    }
+}
